ce74a28ed0b2203b7dc12a55589946b27d961786
Nathaniel Enrique Catalan Eulin <97075463+RedPill-Neo@users.noreply.github.com>
2022-05-23 22:52:59 +0800

    2nd Long Exam (Eulin)

--- 2nd Long Exam/anagrams_eulin.c
+++ 2nd Long Exam/anagrams_eulin.c
@@ -1,39 +1,48 @@
 /*Test III no. 1*/
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <stdbool.h>
+#include <stdio.h> /*for input and output function*/
+#include <stdlib.h> /*for system pausing*/
+#include <ctype.h> /*for toupper and isalpha*/
+#include <stdbool.h> /*for boolean values*/
 
-void scan_word(int occurrences[26]);
+void scan_word(int occurrences[26]); /*declaring required functions*/
 bool is_anagram(int occurrences1[26], int occurrences2[26]);
 
-int i;
+int i; /*global variable i, since it is used many times as the index and for iteration*/
 
-int main(void){
-	int occurrences1[26] = {0},
-	occurrences2[26] = {0};
+int main(void){ 
+	int occurrences1[26] = {0}, /*defining the array for the first word as null set*/
+	occurrences2[26] = {0}; /*array to keep track of the second word as null set*/
 
 	printf("Enter the first word: ");
-	scan_word(occurrences1);
+	scan_word(occurrences1); /*call scan_word to scan how many times a letter has occured in a word*/
 
 	printf("Enter the second word: ");
-	scan_word(occurrences2);
+	scan_word(occurrences2); //to scan the second word
 
-	if(is_anagram(occurrences1, occurrences2)){
-		printf("\n\tThe words are anagrams!\n");
-		system("PAUSE");
+	/*if the is_anagram function returns a boolean true, then execute the following*/
+	if(is_anagram(occurrences1, occurrences2)){ 
+		/*Inform the user that the words are anagrams (has the same array)*/
+		printf("\n\tThe words are anagrams!\n"); 
+		system("PAUSE"); //end main; exit program
 		return 0;
 	}
 	else{
 		printf("\n\tThe words are not anagrams.\n");
-		system("PAUSE");
+		/* if the function is_anagram return a false boolean value, 
+		then inform the user it's not an anagram*/
+		system("PAUSE"); 
 		return 0;
 	}
 }
 
-void scan_word(int occurrences[26]){
+/*DEFINING FUNCTIONS*/
+
+void scan_word(int occurrences[26]){ 
 	char c;
-	while ((c = getchar()) != '\n'){
+	/* scans only a letter in the alphabet, turns it to upper case and adds into the array 
+	of numbers (occurences1 or 2) that a letter (an element) if present be added to the number
+	it is placed, i.e, 2 a's, 3 b's, and no c's is equal to {2, 3, 0...},*/
+	while ((c = getchar()) != '\n'){ 
 		if(isalpha(c)){
 			occurrences[toupper(c) - 'A']++;
 		}
@@ -42,7 +51,9 @@ void scan_word(int occurrences[26]){
 
 
 bool is_anagram(int occurrences1[26], int occurrences2[26]){
-	for(i = 0; i < 26; i++){
+	/*for every element in the array designated for each word, if there is any mismatch
+	then the words will be declared as not an anagram*/
+	for(i = 0; i < 26; i++){ 
 		if(occurrences1[i] != occurrences2[i]){
 			return false;
 		}
--- 2nd Long Exam/anagrams_eulin2.c
+++ 2nd Long Exam/anagrams_eulin2.c
@@ -1,26 +1,24 @@
+/*Test III no. 2*/
+#include <stdio.h> /*for input and output function*/
+#include <stdlib.h> /*for system pausing*/
+#include <ctype.h> /*for toupper and isalpha*/
+#include <stdbool.h> /*for boolean values*/
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <stdbool.h>
-
-void scan_word(int occurrences[26]);
+void scan_word(int occurrences[26]); /*declaring required functions*/
 bool is_anagram(int occurrences1[26], int occurrences2[26]);
 
 int i,
-*ptr1, *ptr2;
+*ptr1, *ptr2; //declaring a pointer global variable
 
-int main(void){
+int main(void){ 
 	int occurrences1[26] = {0},
 	occurrences2[26] = {0};
 
 	printf("Enter the first word: ");
-	scan_word(occurrences1);
+	scan_word(occurrences1); //scan if a letter occured, turn into a number array (occurrences)
 
 	printf("Enter the second word: ");
-	scan_word(occurrences2);
-
-	int word = is_anagram(occurrences1, occurrences2);
+	scan_word(occurrences2); 
 
 	if(is_anagram(occurrences1, occurrences2)){
 		printf("\n\tThe words are anagrams!\n");
@@ -34,8 +32,10 @@ int main(void){
 	}
 }
 
+/*defining functions*/
 void scan_word(int occurrences[26]){
-	char c;
+	char c; 
+	/*turns each occurrences of a letter in each word into a number array*/
 	while ((c = getchar()) != '\n'){
 		if(isalpha(c)){
 			occurrences[toupper(c) - 'A']++;
@@ -49,6 +49,7 @@ bool is_anagram(int occurrences1[26], int occurrences2[26]){
 	ptr2 = occurrences2;
 
 	for(i = 0; i < 26; i++){
+		// using pointer notation, the ptr variables will navigate the array and retrieve elements for comparison
 		if(*(ptr1+i) != *(ptr2+i)){
 			return false;
 		}
